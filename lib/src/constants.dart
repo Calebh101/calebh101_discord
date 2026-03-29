a31a37cca7db0b6c7d0aafd75f78f7dca53c104b
caleb <calebh101dev@icloud.com>
2026-03-28 19:05:20 -0500

    Fix sendMessageAs command

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -30,15 +30,15 @@ Future<String> Function(MessageCreateEvent) prefixFromServerSettings(ServerSetti
   return prefix;
 };
 
-BotCommand sendMessageAs() => BotCommand.command("sendmessage", "Send a message on my behalf.", (ChatContext context, String content, [GuildTextChannel? channel, Message? reply]) async {
+BotCommand sendMessageAs() => BotCommand.command("sendmessage", "Send a message on my behalf.", (ChatContext context, String content, [GuildTextChannel? channel, Snowflake? reply]) async {
   if (await context.assureOwner() == false) return;
   final c = channel ?? context.channel;
   if (c is! GuildTextChannel) return context.respondWithError("The selected channel is not a valid channel.\nExpected: `GuildTextChannel`, got: `${c.runtimeType}`");
   if (context.guild == null) return context.respondWithError("No guild found.");
 
   try {
-    await c.sendMessage(MessageBuilder(content: content, referencedMessage: reply != null ? MessageReferenceBuilder(type: MessageReferenceType.defaultType, messageId: reply.id) : null));
-    await context.respond(MessageBuilder(content: "Message sent to ${c.toMention()}.\n-# Reply ID: ${reply?.id ?? null.toDiscordCodeString()}"), level: ResponseLevel.hint);
+    await c.sendMessage(MessageBuilder(content: content, referencedMessage: reply != null ? MessageReferenceBuilder(type: MessageReferenceType.defaultType, messageId: reply, failIfInexistent: false) : null));
+    await context.respond(MessageBuilder(content: "Message sent to ${c.toMention()}.\n-# Reply ID: ${reply.toDiscordCodeString()}"), level: ResponseLevel.hint);
   } catch (e) {
     Logger.warn("SendMessageAs", "Unable to send message from ${channel?.id}: $e");
     context.respondWithError("Unable to send message.", level: ResponseLevel.private);
