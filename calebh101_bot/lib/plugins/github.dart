e45214d9c3f3aae2ae98843b7c42d7b9b0b82797
caleb <calebh101dev@icloud.com>
2026-05-25 20:23:10 -0500

    GitHub updates

--- calebh101_bot/lib/plugins/github.dart
+++ calebh101_bot/lib/plugins/github.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:calebh101_bot/main.dart';
 import 'package:calebh101_discord/calebh101_discord.dart';
 
 class GitHubPlugin extends BotPluginLegacy {
@@ -8,10 +9,29 @@ class GitHubPlugin extends BotPluginLegacy {
   @override
   FutureOr<List<BotCommand<Function>>> commands<T extends ChatContext>(CommandsPlugin plugin, KVStore store) {
     return [
-      BotCommand("github", "GitHub", "Send a GitHub repo.", (ChatContext context, String arg1, [String? arg2, GreedyStringList? params]) async {
-        final p = params?.data ?? [];
-        await context.respond(MessageBuilder(content: "https://gh.calebh101.net${[arg1, ?arg2].map((x) => "/$x").join("")}${p.isNotEmpty ? "?${p.first}${p.sublist(1, p.length).map((x) => "&$x").join("")}" : ""}"));
+      BotCommand("github", "GitHub", "Send a GitHub repo.", (ChatContext context, String arg1, [String? arg2, GreedyString? addons]) async {
+        await context.respond(MessageBuilder(content: "https://gh.calebh101.net${[arg1, ?arg2].map((x) => "/$x").join("")}${addons?.data ?? ""}"));
       }, aliases: ["gh"]),
+
+      BotCommand("githubchannel", "GitHub", "Get the current channel for GitHub updates.", (T context) async {
+        final settings = GitHubServerSettings(store, context.guild!.id);
+        final id = settings.githubChannel.get();
+
+        await context.respond(MessageBuilder(content: id != null ? "GitHub updates channel: ${id.value.toChannel()}" : "No GitHub updates channel set."));
+      }, needsGuild: true),
+
+      BotCommand("setgithubchannel", "GitHub", "Set the current channel for GitHub updates.", (T context, [GuildTextChannel? channel]) async {
+        final settings = GitHubServerSettings(store, context.guild!.id);
+        settings.githubChannel.set(channel?.id);
+
+        await context.respond(MessageBuilder(content: "GitHub updates channel ${channel != null ? "set to ${channel.toMention()}" : "**reset**"}."));
+      }, needsGuild: true, permissionsRequired: BotCommandPermissions.admin),
     ];
   }
+}
+
+class GitHubServerSettings extends Calebh101BotServerSettings {
+  GitHubServerSettings(super.store, super.id);
+
+  SettingsObject<Snowflake> get githubChannel => SettingsObject.snowflake(this, "githubChannel");
 }
