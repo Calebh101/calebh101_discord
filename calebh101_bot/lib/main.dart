21717fb9a9edede5bdc67120c36e0fe979c8c536
caleb <calebh101dev@icloud.com>
2026-09-16 11:44:53 -0500

    Disable Videos plugin

--- calebh101_bot/lib/main.dart
+++ calebh101_bot/lib/main.dart
@@ -21,7 +21,6 @@ import 'package:calebh101_bot/plugins/selfreact.dart';
 import 'package:calebh101_bot/plugins/roles.dart';
 import 'package:calebh101_bot/plugins/support.dart';
 import 'package:calebh101_bot/plugins/tags.dart';
-import 'package:calebh101_bot/plugins/video.dart';
 import 'package:calebh101_bot/plugins/welcome.dart';
 import 'package:calebh101_bot/plugins/xp.dart';
 import 'package:calebh101_discord/calebh101_discord.dart';
@@ -78,7 +77,6 @@ void main(List<String> arguments) => wrap(() async {
     SupportPlugin(),
     StatusPlugin(),
     BlindPlugin(),
-    VideoPlugin(),
     MultiplayerPlugin(),
 
     GuessTheNumberPlugin(),
